f3a44f332844db23a2baead01bc24f33dc9de49d
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-03-02 00:00:58 -0500

    v1.0.0

---
+++ tic-tac-toe-app/src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; } button:focus{outline: 0 !important; box-shadow: none !important;}']
+})
+export class SquareComponent {
+  @Input() value!: 'X' | 'O';
+}
