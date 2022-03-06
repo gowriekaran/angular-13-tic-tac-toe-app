5339e57d8363ac0fd2de8bb57fb2e9e24c74ba1c
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-03-06 01:28:13 -0500

    v1.1.1
    App name update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-tic-tac-toe-app'),
+      dir: require('path').join(__dirname, './coverage/angular-13-tic-tac-toe-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
